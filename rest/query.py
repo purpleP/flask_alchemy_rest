d094cea1cfd37d3334e1725a4a2e3413487b63ee
Михаил Доронин <warrior2031@mail.ru>
2016-01-12 23:42:51 +0300

    WIP

--- rest/query.py
+++ rest/query.py
@@ -51,19 +51,36 @@ def foreign_key_query(db_session, query_params):
     return query.filter(getattr(model_to_query, query_params[0].foreign_key_name) == filter_value)
 
 
-def query_item(db_session, model, attr_to_query, attr_to_compare, value_to_compare):
-    query = query_coll(db_session, model, attr_to_query)
-    return query.filter(getattr(model, attr_to_compare) == value_to_compare)
+def filter_query(query, model, filter_attr, filter_value):
+    return query.filter(getattr(model, filter_attr), filter_value)
 
 
-def query_coll(db_session, model, attr_to_query):
-    return db_session.query(model)
-
-
-def query_sub(db_session, params):
-    if len(params) == 0:
-        model, fk_attr, parent_key, parent_key_value = params[0]
-        filter = query_item(db_session, model, pa)
+def create_q(db_session, models, params):
+    model = models[-1]
+    exposed_attr, fk_attr, fk_to_attr = params[-1]
+    f = partial(filter_query, model=model, filter_attr=exposed_attr)
+    if len(models) == 1: 
+        cq = db_session.query(model)
+        iq = partial(f, query=cq)
+        return cq, iq, f
     else:
-        model, fk_value, attribute = other_params[0]
-        filter = query_item(db_session, model, fk_value, attribute, other_params[1:])
+        parent_model = models[-2]
+        _cq, _iq, _f = create_query(db_session, models[1:], params[1:])
+        cq = db_session.query(model).filter(
+            getattr(model, fk_attr) == _f(db_session.query(getattr(parent_model, fk_to_attr)))
+        )
+        iq = partial(f, query=cq)
+
+
+
+root_colq = db_session.query(Root)
+root_item_query = root_colq.filter(Root.name == 'root')
+level1_colq = db_session.query(Level1).filter(Level1.root_pk ==
+                                db_session.query(Root.name).filter(Root.name == 'root')
+                                )
+level1_itemq = level1_colq.filter(Level.name == 'level1')
+level2_colq = db_session.query(Level2).filter(
+    Level2.level1_pk == db_session.query(Level1.name).filter(Level1.root_pk ==
+                                db_session.query(Root.name).filter(Root.name == 'root')
+                                )
+)
