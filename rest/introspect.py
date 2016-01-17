da593e5b5c9eaf46cafba1af36a8fe2fac10277f
Михаил Доронин <warrior2031@mail.ru>
2016-01-17 23:42:41 +0300

    WIP

--- rest/introspect.py
+++ rest/introspect.py
@@ -8,8 +8,8 @@ RELATION_BLACKLIST = ('query', 'query_class', '_sa_class_manager',
 
 
 def get_rels(model):
-    attrs_relations = ((attr_name, get_relation_class_for(model, attr_name)) for
-                       attr_name in dir(model))
+    attrs_relations = ((attr_name, get_relation_class_for(model, attr_name))
+                       for attr_name in dir(model))
     return [(attr_name, relation_class) for attr_name, relation_class in
             attrs_relations if relation_class]
 
@@ -39,18 +39,19 @@ def fk_attr_for(child_model, parent_model):
     filter(dir(child_model))
     pass
 
+
 def is_fk(model, attr):
     cols = getattr(model, attr)._parentmapper._cols_by_table
+
 
 def is_fk_col(column):
     return hasattr(column, 'foreign_keys')
 
 
-
 def pk_attr_name(model):
     attrs_names = (attr_name for attr_name in dir(model)
                    if (not attr_name.startswith('__') and
-                       not attr_name in RELATION_BLACKLIST))
+                       attr_name not in RELATION_BLACKLIST))
     return find(partial(is_pk, model), attrs_names)
 
 
